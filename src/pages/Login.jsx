5997519c998c7baaa2a82b921f1b6a3f2fe8ac2f
Filipe Brochier <filipe.brochier@gmail.com>
2021-11-08 16:35:49 -0300

    Requisito 5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  function verifyLogin() {
+    const VALID_EMAIL = /\S+@\S+\.\S+/;
+    const MIN_LENGTH = 6;
+    return !(password.length > MIN_LENGTH && VALID_EMAIL.test(email));
+  }
+
   return (
     <form>
       <h1>Login</h1>
@@ -8,15 +17,18 @@ export default function Login() {
         type="email"
         placeholder="Digite seu email"
         data-testid="email-input"
+        onChange={ (e) => setEmail(e.target.value) }
       />
       <input
         type="password"
         data-testid="password-input"
         placeholder="Digite sua senha"
+        onChange={ (e) => setPassword(e.target.value) }
       />
       <button
         type="button"
         data-testid="login-submit-btn"
+        disabled={ verifyLogin() }
       >
         Entrar
       </button>
