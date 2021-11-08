6699512803a2c29940f2eb44ffa332650df77028
Filipe Brochier <filipe.brochier@gmail.com>
2021-11-08 16:20:51 -0300

    Requisitos 2, 3 e 4

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 export default function Login() {
   return (
-    <div>
-      Login
-    </div>
+    <form>
+      <h1>Login</h1>
+      <input
+        type="email"
+        placeholder="Digite seu email"
+        data-testid="email-input"
+      />
+      <input
+        type="password"
+        data-testid="password-input"
+        placeholder="Digite sua senha"
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Entrar
+      </button>
+    </form>
   );
 }
