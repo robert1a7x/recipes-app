12ca3ea019335288517290b374a5bdd438e4a2ed
Filipe Brochier <filipe.brochier@gmail.com>
2021-11-09 12:52:15 -0300

    Requisito 20

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../Footer.css';
+
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 export default function Footer() {
   return (
-    <footer data-testid="footer">
-      <Link to="/bebidas" data-testid="drinks-bottom-btn">
-        <img src="../images/drinkIcon.svg" alt="drink-icon" />
+    <footer className="footer" data-testid="footer">
+      <Link
+        to="/bebidas"
+        data-testid="drinks-bottom-btn"
+        src={ drinkIcon }
+      >
+        <img src={ drinkIcon } alt="drink-icon" />
       </Link>
-      <Link to="/explorar" data-testid="explore-bottom-btn">
-        <img src="../images/exploreIcon.svg" alt="explore-icon" />
+      <Link
+        to="/explorar"
+        data-testid="explore-bottom-btn"
+        src={ exploreIcon }
+      >
+        <img src={ exploreIcon } alt="explore-icon" />
       </Link>
-      <Link to="/comidas" data-testid="food-bottom-btn">
-        <img src="../images/mealIcon.svg" alt="meal-icon" />
+      <Link
+        to="/comidas"
+        data-testid="food-bottom-btn"
+        src={ mealIcon }
+      >
+        <img src={ mealIcon } alt="meal-icon" />
       </Link>
     </footer>
   );
