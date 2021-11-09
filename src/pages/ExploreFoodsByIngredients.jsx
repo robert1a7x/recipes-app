2494931ae672454c57f3779f1f1f3e7f70df07e6
Filipe Brochier <filipe.brochier@gmail.com>
2021-11-09 11:45:11 -0300

    Requisitos 19, 21-24

--- src/pages/ExploreFoodsByIngredients.jsx
+++ src/pages/ExploreFoodsByIngredients.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function ExploreFoodsByIngredients() {
   return (
     <div>
       <Header title="Explorar Ingredientes" />
-      Tela de explorar comidas por igredientes
+      Tela de explorar comidas por ingredientes
+      <Footer />
     </div>
   );
 }
