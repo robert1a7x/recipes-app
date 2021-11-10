48ac259c706529036a264075c74c3b6361e9731c
Filipe Brochier <filipe.brochier@gmail.com>
2021-11-10 09:48:01 -0300

    Ajustes para requisitos 10, 11 e 21

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,11 +4,13 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const email = user ? user.email : '';
   return (
     <div>
       <Header title="Perfil" />
       <h2 data-testid="profile-email">
-        { JSON.parse(localStorage.getItem('user')).email }
+        { email }
       </h2>
       <Link to="/receitas-feitas">
         <button type="button" data-testid="profile-done-btn">
