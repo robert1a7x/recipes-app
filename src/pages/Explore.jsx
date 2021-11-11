262ff138fde3358ccd70df5ac87a27994c6c13ec
Filipe Brochier <filipe.brochier@gmail.com>
2021-11-11 12:16:43 -0300

    Requisitos 67 e 68

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -6,7 +7,19 @@ export default function Explore() {
   return (
     <div>
       <Header title="Explorar" />
-      Tela de explorar
+      {/* <Link to="/explorar/comidas"> */}
+      <button
+        type="button"
+        data-testid="explore-food"
+      >
+        Explorar Comidas
+      </button>
+      <button
+        type="button"
+        data-testid="explore-drinks"
+      >
+        Explorar Bebidas
+      </button>
       <Footer />
     </div>
   );
