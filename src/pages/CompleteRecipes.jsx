8b6b160815a865a67ab3ab21b10d388bee674dcd
Robert Nascimento <robert1a7x@gmail.com>
2021-11-16 19:40:22 -0300

    Removido o array hardcoded para testes no navegador

--- src/pages/CompleteRecipes.jsx
+++ src/pages/CompleteRecipes.jsx
@@ -2,38 +2,13 @@ import React, { useState, useEffect } from 'react';
 import DoneRecipeCard from '../components/DoneRecipeCard';
 import Header from '../components/Header';
 
-const doneRecipesArray = [
-  {
-    id: '52771',
-    type: 'comida',
-    area: 'Italian',
-    category: 'Vegetarian',
-    alcoholicOrNot: '',
-    name: 'Spicy Arrabiata Penne',
-    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-    doneDate: '23/06/2020',
-    tags: ['Pasta', 'Curry'],
-  },
-  {
-    id: '178319',
-    type: 'bebida',
-    area: '',
-    category: 'Cocktail',
-    alcoholicOrNot: 'Alcoholic',
-    name: 'Aquamarine',
-    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-    doneDate: '23/06/2020',
-    tags: [],
-  },
-];
-
 export default function CompleteRecipes() {
   const [doneRecipes, setDoneRecipes] = useState([]);
   const recipes = JSON.parse(localStorage.getItem('doneRecipes'));
 
   useEffect(() => {
     if (!localStorage.doneRecipes) {
-      localStorage.setItem('doneRecipes', JSON.stringify([doneRecipesArray]));
+      localStorage.setItem('doneRecipes', JSON.stringify([]));
     }
     setDoneRecipes(recipes);
   }, []);
